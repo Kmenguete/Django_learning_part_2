df7515a14a4010e53cf4b178656c44d1eb5de662
Kmenguete <menguetekevin@gmail.com>
2022-05-25 11:05:14 +0200

    creating a personnalized user model

--- fotoblog/fotoblog/authentification/models.py
+++ fotoblog/fotoblog/authentification/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = ((CREATOR, 'Creator'),
+                    (SUBSCRIBER, 'Subscriber'),)
+    profile_photo = models.ImageField(verbose_name='Profile photo')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Role')
